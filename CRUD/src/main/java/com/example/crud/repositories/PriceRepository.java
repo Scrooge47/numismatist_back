0ae09f090714339c3d8620061e4cbe1bcdaf4ca7
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-19 23:15:21 +0300

    Change key type

--- CRUD/src/main/java/com/example/crud/repositories/PriceRepository.java
+++ CRUD/src/main/java/com/example/crud/repositories/PriceRepository.java
@@ -1,9 +1,10 @@
 package com.example.crud.repositories;
 
 import com.example.crud.models.Price;
+import com.example.crud.models.PriceKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
+public interface PriceRepository extends JpaRepository<Price, PriceKey> {
 }
